444ea63444a75a0958d09fd8459d0cb499e4110b
michele-marrandino <michele.marrandino@infocube.it>
2024-03-15 12:11:08 +0100

    Minor fixes on Permissions

--- src/main/java/org/fhir/auth/irccs/entity/Permission.java
+++ src/main/java/org/fhir/auth/irccs/entity/Permission.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Permission {
     private String resource;
     public List<String> permissions = new ArrayList<>();
+    public List<String> disabled = new ArrayList<>();
 
     public void setResource(String resource) {
         this.resource = resource;
@@ -17,35 +18,47 @@ public class Permission {
     public void setCreate(Boolean create) {
         if(create){
             this.permissions.add("create");
+        } else {
+            this.disabled.add("create");
         }
     }
 
     public void setRead(Boolean read) {
         if(read){
             this.permissions.add("read");
+        } else {
+            this.disabled.add("read");
         }
     }
     public void setUpdate(Boolean update) {
         if(update){
             this.permissions.add("update");
+        } else {
+            this.disabled.add("update");
         }
     }
 
     public void setSearch(Boolean search) {
         if(search){
             this.permissions.add("search");
+        } else {
+            this.disabled.add("search");
         }
     }
 
     public void setHistory(Boolean history) {
         if(history){
             this.permissions.add("history");
+        } else {
+            this.disabled.add("history");
         }
     }
 
     public void setDelete(Boolean delete) {
         if(delete){
             this.permissions.add("delete");
+        } else {
+            this.disabled.add("delete");
         }
     }
 
@@ -55,5 +68,9 @@ public class Permission {
 
     public String getResource() {
         return resource;
+    }
+
+    public List<String> getDisabled() {
+        return disabled;
     }
 }
--- src/main/java/org/fhir/auth/irccs/service/PermissionService.java
+++ src/main/java/org/fhir/auth/irccs/service/PermissionService.java
@@ -1,28 +1,23 @@
 package org.fhir.auth.irccs.service;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.fhir.auth.irccs.entity.Permission;
 import org.fhir.auth.irccs.entity.PermissionWrapper;
-import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.*;
-import org.keycloak.representations.idm.GroupRepresentation;
-import org.keycloak.representations.idm.RoleRepresentation;
+import org.keycloak.admin.client.token.TokenManager;
 import org.keycloak.representations.idm.authorization.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @ApplicationScoped
@@ -115,13 +110,7 @@ public class PermissionService {
 
     // Move it to a Fhir centered-class static
     private Response createResource(String groupName, String resourceName, String type, AuthorizationResource authzResource) throws Exception {
-        ResourceRepresentation resource = new ResourceRepresentation();
-        String calculatedName = String.format("%s - %s: %s Resource", type.toUpperCase(), groupName.toUpperCase(), resourceName);
-        resource.setName(calculatedName);
-        resource.setDisplayName(calculatedName);
-
-        ResourcePermissionRepresentation resourcePermission = new ResourcePermissionRepresentation();
-        String policyName = String.format("%s Policy - %s", type.toUpperCase(), groupName.toUpperCase());
+        String policyName = String.format("Policy - %s", groupName.toUpperCase());
         String groupId;
         String adminGroupId;
 
@@ -132,59 +121,32 @@ public class PermissionService {
             LOG.error("ERROR: Couldn't find group {}", groupName, e);
             throw new Exception("ERROR: Couldn't find group " + groupName);
         }
-
-        switch (type.toLowerCase()) {
-            case "create":
-                resource.addScope("POST");
-                resource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resourceName)));
-                break;
-            case "read":
-                resource.addScope("GET");
-                resource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resourceName + "/*")));
-                break;
-            case "update":
-                resource.addScope("PUT");
-                resource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resourceName + "/*")));
-                break;
-            case "delete":
-                resource.addScope("DELETE");
-                resource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resourceName + "/*")));
-                break;
-            case "search":
-                resource.addScope("GET");
-                resource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resourceName + "/_search")));
-                break;
-            case "history":
-                resource.addScope("GET");
-                resource.setUris(new HashSet<>(List.of(
-                        "/fhir/" + resourceName + "/_history",
-                        "/fhir/" + resourceName + "/{id}/_history",
-                        "/fhir/" + resourceName + "/{id}/_history/{version_id}"
-                )));
-                break;
-            default:
-                throw new IllegalArgumentException("Invalid resource type: " + type);
-        }
-
         ensureGroupPolicyExists(policyName, groupId, adminGroupId, authzResource);
+        String calculatedName = String.format("%s Resource", resourceName);
+        ensureResourcesExists(calculatedName, authzResource, resourceName);
 
-        resourcePermission.setName(calculatedName + " Permission");
-        resourcePermission.addPolicy(policyName);
-
-        Response resourceCreation = authzResource.resources().create(resource);
-        resourcePermission.addResource(authzResource.resources().findByName(calculatedName).get(0).getId());
-        Response resourcePermissionCreation = authzResource.permissions().resource().create(resourcePermission);
-
-        resourceCreation.close();
-        resourcePermissionCreation.close();
-
-        return resourcePermissionCreation;
+        return ensurePermissionExists(resourceName, policyName, type, authzResource, groupId);
     }
 
     public Response createPermission(PermissionWrapper permission) {
         AuthorizationResource authzResource = getRealm().clients().get(getRealm().clients().findByClientId(clientId).get(0).getId()).authorization();
 
         for (Permission p : permission.getPermissions()) {
+
+            for (String scopeType : p.getDisabled()) {
+                try {
+                    String groupId = getRealm().groups().groups(permission.getGroupName(),  0,  1, false).get(0).getId();
+                    String calculatedName = String.format("%s: %s Permission", scopeType.toUpperCase(), p.getResource());
+                    authzResource.permissions().resource();
+                    ScopePermissionResource permissionResource = authzResource.permissions().scope().findById(authzResource.permissions().scope().findByName(calculatedName).getId());
+                    List<PolicyRepresentation> policies = permissionResource.associatedPolicies();
+                    policies.removeIf(x -> x.getDescription().equals(groupId));
+                    ScopePermissionRepresentation scopePermissionRepresentation = permissionResource.toRepresentation();
+                    scopePermissionRepresentation.setPolicies(policies.stream().map(AbstractPolicyRepresentation::getName).collect(Collectors.toSet()));
+                    permissionResource.update(scopePermissionRepresentation);
+                } catch (Exception e){LOG.debug("Couldn't find permission policy to delete");}
+            }
+
             for (String scopeType : p.getPermissions()) {
                 try {
                     createResource(permission.getGroupName(), p.getResource(), scopeType, authzResource).close();
@@ -197,16 +159,128 @@ public class PermissionService {
         return Response.ok().build();
     }
 
-    private void ensureGroupPolicyExists(String policyName, String groupId, String adminGroupId, AuthorizationResource authzResource) {
+    private Response ensureGroupPolicyExists(String policyName, String groupId, String adminGroupId, AuthorizationResource authzResource) {
+        Response policy = null;
+
         try {
-            authzResource.policies().group().findByName(policyName).getId();
+            policy = Response.ok(authzResource.policies().group().findById(authzResource.policies().group().findByName(policyName).getId())).build();
         } catch (Exception e) {
             GroupPolicyRepresentation groupPolicyRepresentation = new GroupPolicyRepresentation();
             groupPolicyRepresentation.setName(policyName);
-            groupPolicyRepresentation.addGroup(groupId, adminGroupId);
-            authzResource.policies().group().create(groupPolicyRepresentation).close();
+            groupPolicyRepresentation.addGroup(groupId);
+            groupPolicyRepresentation.setDescription(groupId);
+            policy = authzResource.policies().group().create(groupPolicyRepresentation);
+            policy.close();
         }
+        return policy;
     }
+
+    private Response ensureResourcesExists(String resourceName, AuthorizationResource authzResource, String resource) {
+        Response res = null;
+        try {
+            res = Response.ok(authzResource.resources().findByName(resourceName + " BASE").get(0)).build();
+        } catch (Exception e) {
+            ResourceRepresentation baseResource = new ResourceRepresentation();
+            String baseResourceName = resourceName + " BASE";
+            baseResource.setName(baseResourceName);
+            baseResource.setDisplayName(baseResourceName);
+            baseResource.addScope("GET", "PUT", "DELETE");
+            baseResource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resource + "/{id}")));
+
+            authzResource.resources().create(baseResource).close();
+
+            ResourceRepresentation historyResource = new ResourceRepresentation();
+            String historyResourceName = resourceName + " HISTORY";
+            historyResource.setName(historyResourceName);
+            historyResource.setDisplayName(historyResourceName);
+            historyResource.addScope("GET");
+            historyResource.setUris(new HashSet<>(List.of(
+                    "/fhir/" + resource + "/_history",
+                    "/fhir/" + resource + "/{id}/_history",
+                    "/fhir/" + resource + "/{id}/_history/{version_id}"
+            )));
+
+            authzResource.resources().create(historyResource).close();
+
+            ResourceRepresentation createResource = new ResourceRepresentation();
+            String createResourceName = resourceName + " CREATE";
+            createResource.setName(createResourceName);
+            createResource.setDisplayName(createResourceName);
+            createResource.addScope("POST");
+            createResource.setUris(new HashSet<>(Collections.singleton("/fhir/" + resource)));
+
+            authzResource.resources().create(createResource).close();
+
+            ResourceRepresentation searchResource = new ResourceRepresentation();
+            String searchResourceName = resourceName + " SEARCH";
+            searchResource.setName(searchResourceName);
+            searchResource.setDisplayName(searchResourceName);
+            searchResource.addScope("GET");
+            searchResource.setUris(new HashSet<>(List.of("/fhir/" + resource, "/fhir/" + resource + "/_search")));
+            authzResource.resources().create(searchResource).close();
+        }
+        return res;
+    }
+
+    private Response ensurePermissionExists(String resourceName, String policyName, String type, AuthorizationResource authzResource, String groupId) {
+        Response resourcePermissionCreation = null;
+        List<String> policiesAssociated = new ArrayList<>();
+        String calculatedName = String.format("%s: %s Permission", type.toUpperCase(), resourceName);
+        try {
+            policiesAssociated.addAll(authzResource.permissions().scope().findById(authzResource.permissions().scope().findByName(calculatedName).getId()).associatedPolicies().stream().map(AbstractPolicyRepresentation::getName).toList());
+            authzResource.permissions().scope().findById(authzResource.permissions().scope().findByName(calculatedName).getId()).remove();
+        }
+        catch (Exception e){
+            LOG.info("Couldn't locate permission", e);
+        } finally {
+            ScopePermissionRepresentation resourcePermission = new ScopePermissionRepresentation();
+            resourcePermission.setName(String.format("%s: %s Permission", type.toUpperCase(), resourceName));
+            policiesAssociated.add(authzResource.policies().group().findByName(policyName).getName());
+            policiesAssociated.add("Admin Policy");
+            resourcePermission.addPolicy(policiesAssociated.toArray(new String[0]));
+            String scope;
+            resourcePermission.setDecisionStrategy(DecisionStrategy.AFFIRMATIVE);
+
+            String findResource;
+
+            switch (type.toLowerCase()) {
+                case "create":
+                    findResource = String.format("%s Resource CREATE", resourceName);
+                    scope = "POST";
+                    break;
+                case "read":
+                    findResource = String.format("%s Resource BASE", resourceName);
+                    scope = "GET";
+                    break;
+                case "update":
+                    findResource = String.format("%s Resource BASE", resourceName);
+                    scope = "PUT";
+                    break;
+                case "delete":
+                    findResource = String.format("%s Resource BASE", resourceName);
+                    scope = "DELETE";
+                    break;
+                case "search":
+                    findResource = String.format("%s Resource SEARCH", resourceName);
+                    scope = "GET";
+                    break;
+                case "history":
+                    findResource = String.format("%s Resource HISTORY", resourceName);
+                    scope = "GET";
+                    break;
+                default:
+                    throw new IllegalArgumentException("Invalid resource type: " + type);
+            }
+
+            resourcePermission.addScope(scope);
+            resourcePermission.addResource(authzResource.resources().findByName(findResource).get(0).getId());
+            resourcePermissionCreation = authzResource.permissions().scope().create(resourcePermission);
+            resourcePermissionCreation.close();
+        }
+        return resourcePermissionCreation;
+    }
+
+
 
 }
 
