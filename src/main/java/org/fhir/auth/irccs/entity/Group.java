ea22ce26c13aa969b751892ec9659e07bf8b533c
michele-marrandino <michele.marrandino@infocube.it>
2024-03-19 15:36:43 +0100

    Minor fixes on Tests

--- src/main/java/org/fhir/auth/irccs/entity/Group.java
+++ src/main/java/org/fhir/auth/irccs/entity/Group.java
@@ -91,8 +91,7 @@ public class Group {
         }
         if(group.getMembers().size() > 0){
             String finalCreatedId = createdId;
-            List<String> users = group.getMembers().stream().map(user -> realm.users().get(user).toRepresentation().getId()).toList();
-            users.forEach(member -> realm.users().get(member).joinGroup(finalCreatedId) );
+            group.getMembers().forEach(member -> realm.users().get(member).joinGroup(finalCreatedId) );
         }
         if(group.getOrganizations().size() > 0){
             groupRepresentation.setAttributes(new HashMap<>(){{
