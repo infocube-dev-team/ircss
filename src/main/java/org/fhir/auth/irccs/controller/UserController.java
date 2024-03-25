95c398f627157c03ab63f36f49c13875c726fed7
michele-marrandino <michele.marrandino@infocube.it>
2024-03-25 12:49:16 +0100

    Revert "Minor fixes"

--- src/main/java/org/fhir/auth/irccs/controller/UserController.java
+++ src/main/java/org/fhir/auth/irccs/controller/UserController.java
@@ -1,11 +1,13 @@
 package org.fhir.auth.irccs.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import io.quarkus.security.Authenticated;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
 import org.fhir.auth.irccs.entity.User;
+import org.keycloak.representations.AccessTokenResponse;
 
 import java.util.HashMap;
 
@@ -38,6 +40,9 @@ public interface UserController {
     @Consumes("application/x-www-form-urlencoded")
     @POST
     Response logout(String payload);
+    @Path("/enable")
+    @POST
+    Response enableUser(@QueryParam("email") @DefaultValue("") String email);
     @PUT
     Response updateUser(User user);
 
--- src/main/java/org/fhir/auth/irccs/service/GroupService.java
+++ src/main/java/org/fhir/auth/irccs/service/GroupService.java
@@ -3,15 +3,19 @@ package org.fhir.auth.irccs.service;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MultivaluedHashMap;
+import jakarta.ws.rs.core.MultivaluedMap;
 import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.fhir.auth.irccs.RollbackSystem.Command;
 import org.fhir.auth.irccs.RollbackSystem.RollbackManager;
+import org.fhir.auth.irccs.entity.User;
 import org.fhir.auth.irccs.exceptions.OperationException;
-import org.hl7.fhir.r5.model.Group;
-import org.hl7.fhir.r5.model.Identifier;
-import org.hl7.fhir.r5.model.OperationOutcome;
-import org.hl7.fhir.r5.model.Reference;
+import org.hl7.fhir.instance.model.api.IIdType;
+import org.hl7.fhir.r5.model.*;
+import org.jboss.resteasy.reactive.RestResponse;
+import org.jboss.resteasy.reactive.client.impl.ClientResponseImpl;
+import org.keycloak.admin.client.CreatedResponseUtil;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.resource.GroupResource;
 import org.keycloak.admin.client.resource.GroupsResource;
@@ -22,11 +26,10 @@ import org.quarkus.irccs.client.restclient.FhirClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ApplicationScoped
@@ -124,13 +127,55 @@ public class GroupService {
         }
     }
 
-    private Response getGroupByName(String name) {
-        List<GroupRepresentation> groups = getRealm().groups().groups(name, 0, 1, false);
-        if(groups.size() > 0){
-            return Response.status(200).entity(groups.get(0)).build();
+    /*public Response createGroup(org.fhir.auth.irccs.entity.Group group) {
+        // Creating Keycloak Group Representation
+
+        GroupRepresentation groupRepresentation = new GroupRepresentation();
+        groupRepresentation.setName(group.getName());
+
+        GroupsResource groupsResource = getRealm().groups();
+        Response KCgroupCreated = groupsResource.add(groupRepresentation);
+
+        String keycloakGroupId = CreatedResponseUtil.getCreatedId(KCgroupCreated);
+
+
+        GroupRepresentation foundGroup = groupsResource.groups(group.getName(), 0, 1).get(0);
+        Objects.requireNonNull(foundGroup);
+
+
+        List<Group.GroupMemberComponent> practitionerReferences = new ArrayList<>();
+        for(String email : group.getMembers()){
+            userService.joinGroup(email, foundGroup);
+            try {
+                practitionerReferences.add(new Group.GroupMemberComponent().setEntity(new Reference(userService.getUserByEmail_fhir(email).getId())));
+            } catch (Exception e){
+                e.printStackTrace();
+                throw new OperationException("Couldn't find FHIR practitioner", OperationOutcome.IssueSeverity.ERROR);
+            }
         }
 
-        return Response.status(404).entity(groups).build();
+        Group fhirGroup = new Group();
+        fhirGroup.setName(group.getName());
+        fhirGroup.setMember(practitionerReferences);
+        fhirGroup.setIdentifier(List.of(new Identifier().setUse(Identifier.IdentifierUse.SECONDARY).setValue(keycloakGroupId)));
+
+        try {
+            IIdType groupCreated = groupController.create(fhirGroup);
+            LOG.info("Group created: {}", groupCreated);
+        } catch (Exception e){
+            groupsResource.group(keycloakGroupId).remove();
+        }
+
+        return Response.ok().status(Response.Status.CREATED).build();
+
+    }*/
+
+    private Response getGroupByName(String name) {
+        return Response.ok(org.fhir.auth.irccs.entity.Group.fromGroupRepresentation(getGroupByName_representation(name), getRealm())).build();
+    }
+
+    public GroupRepresentation getGroupByName_representation(String name) {
+        return getRealm().groups().groups(name, 0, 1, false).get(0);
     }
 
     public Response updateGroup(org.fhir.auth.irccs.entity.Group group) {
