f51b06c0eb8edcc7b9384921a8de7714d6bad5d9
michele-marrandino <michele.marrandino@infocube.it>
2025-01-21 15:07:17 +0100

    PASTRL-322

--- src/main/java/org/fhir/auth/irccs/service/UserService.java
+++ src/main/java/org/fhir/auth/irccs/service/UserService.java
@@ -197,7 +197,9 @@ public class UserService {
                 UserRepresentation userRepresentation = User.toUserRepresentation(user);
                 Response response = usersResource.create(userRepresentation);
                 user.setId(CreatedResponseUtil.getCreatedId(response));
-                getRealm().users().get(user.getId()).joinGroup(user.getOrganizationRequest().get(0));
+                if( null != user.getOrganizationRequest()){
+                    getRealm().users().get(user.getId()).joinGroup(user.getOrganizationRequest().get(0));
+                }
                 Objects.requireNonNull(user.getId(), "User ID cannot be null after creation.");
 
                 LOG.info("Keycloak User created: " + user.getEmail() + ". Send reset password...");
